66f513269ed0efad0f85a6fa5c799e7bf2f7d994
Ahammad Shawki <ahammadshawki8@gmail.com>
2021-02-18 04:50:15 +0000

    Changed the code

--- main.py
+++ main.py
@@ -1,23 +1,58 @@
+# Importing Necessary Libraries
 import tweepy
 import database
 import time
 
+
+# Setting Up Few Properties
 CONSUMER_KEY = ""
 CONSUMER_SECRET = ""
 ACCESS_KEY = ""
 ACCESS_SECRET = ""
 TAGS = ["#100DaysOfCode", "#CodeNewbie", "#Python", "#EddieHub", "#DEVCommunity", "#Quote"]
+ITEMS = 200
+SLEEP_PER_ITEM = 5
+SLEEP_PER_ROUND = 5*60
+DELETE_AFTER_COUNT = 10
 
+
+# Accessing the API
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
-parent = list()
-mentions = api.mentions_timeline()
-home = [x for x in api.home_timeline() if any(TAGS) in x.text]
 
-parent.extend(mentions)
-parent.extend(home)
+# Main Function
+def run_bot():
+  parent = list()
+  mentions = api.mentions_timeline()
+  search = []
+  for tag in TAGS:
+    search.extend(tweepy.Cursor(api.search, tag).items(ITEMS))
 
-for item in parent:
-    pass
+  parent.extend(mentions)
+  parent.extend(search)
+
+  for tweet in parent:
+    if not (str(tweet.id) in database.read_info()):
+      api.create_favourite(tweet.id)
+      api.retweet(tweet.id)
+      if "tweepy.models.Status" in type(tweet):
+        api.update_status("""
+        Thanks for mentioning me. 
+        Your tweet has been liked and replied. Please folow my creator @AhammadShawki8 💙
+        """, tweet.id)
+      database.write_item(tweet.id)
+      time.sleep(SLEEP_PER_ITEM)
+
+
+
+# Running the program
+if __name__ == "__main__":
+  counter = 0
+  while True:
+    run_bot()
+    time.sleep(SLEEP_PER_ROUND)
+    if counter == DELETE_AFTER_COUNT:
+      database.clear_info()
+    counter += 1
